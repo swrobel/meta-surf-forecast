31000988ddb4fd938b8d4220f98e665491291e90
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-01-23 21:11:20 -0800

    Pacific time

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module MetaSurfForecast
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
