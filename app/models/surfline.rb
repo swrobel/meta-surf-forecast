836a13826a3287d9a7ca76e1d686db131c89c22c
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-02-13 21:43:43 -0800

    More efficient surfline response parsing

--- app/models/surfline.rb
+++ app/models/surfline.rb
@@ -5,33 +5,40 @@ class Surfline < Forecast
     end
 
     def parse_response(spot, response)
+      forecasts = {}
+
       response.Surf.dateStamp.each_with_index do |day, day_index|
         day.each_with_index do |timestamp, timestamp_index|
-          record = self.where(spot_id: spot.id, timestamp: Time.zone.parse(timestamp)).first_or_initialize
-          record.min_height = response.Surf.surf_min[day_index][timestamp_index]
-          record.max_height = response.Surf.surf_max[day_index][timestamp_index]
-          record.save
+          forecast = forecasts[Time.zone.parse(timestamp)] = {}
+          forecast[:min_height] = response.Surf.surf_min[day_index][timestamp_index]
+          forecast[:max_height] = response.Surf.surf_max[day_index][timestamp_index]
         end
       end
 
       response.Sort.dateStamp.each_with_index do |day, day_index|
         day.each_with_index do |timestamp, timestamp_index|
           max_swell_rating = 0
+          forecast = forecasts[Time.zone.parse(timestamp)] = {}
           (1..6).each do |swell_index|
             max_swell_rating = [max_swell_rating, response.Sort["optimal#{swell_index}"][day_index][timestamp_index].to_d].max
           end
-          record = self.where(spot_id: spot.id, timestamp: Time.zone.parse(timestamp)).first_or_initialize
-          record.swell_rating = max_swell_rating
-          record.save
+          forecast[:swell_rating] = max_swell_rating
         end
       end
 
       response.Wind.dateStamp.each_with_index do |day, day_index|
         day.each_with_index do |timestamp, timestamp_index|
-          record = self.where(spot_id: spot.id, timestamp: Time.zone.parse(timestamp)).first_or_initialize
-          record.optimal_wind = response.Wind.optimalWind[day_index][timestamp_index]
-          record.save
+          forecast = forecasts[Time.zone.parse(timestamp)] = {}
+          forecast[:optimal_wind] = response.Wind.optimalWind[day_index][timestamp_index]
         end
+      end
+
+      forecasts.each do |key, values|
+        record = self.where(spot_id: spot.id, timestamp: key).first_or_initialize
+        values.each do |key, value|
+          record[key] = value
+        end
+        record.save!
       end
     end
   end
