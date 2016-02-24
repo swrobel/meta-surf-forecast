335077f4b515db8d42c898ea885bdbbb2a0d17f8
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-02-23 23:54:48 -0800

    Surfline can now handle multiple spots in response

--- app/models/surfline.rb
+++ app/models/surfline.rb
@@ -4,42 +4,53 @@ class Surfline < Forecast
       "http://api.surfline.com/v1/forecasts/#{spot.surfline_id}?resources=surf,analysis,wind,weather,tide,sort&days=30&getAllSpots=false&units=e&interpolate=true&showOptimal=true&usenearshore=true"
     end
 
-    def parse_response(spot, request, responses)
+    def parse_response(_spot, request, responses)
+      # If getAllSpots=false, response will be a single object instead of an array
+      responses = [responses] unless responses.is_a? Array
       forecasts = {}
 
-      responses.Surf.dateStamp.each_with_index do |day, day_index|
-        day.each_with_index do |timestamp, timestamp_index|
-          forecast = forecasts[Time.zone.parse(timestamp)] = {}
-          forecast[:min_height] = responses.Surf.surf_min[day_index][timestamp_index]
-          forecast[:max_height] = responses.Surf.surf_max[day_index][timestamp_index]
-        end
-      end
-
-      responses.Sort.dateStamp.each_with_index do |day, day_index|
-        day.each_with_index do |timestamp, timestamp_index|
-          max_swell_rating = 0
-          forecast = forecasts[Time.zone.parse(timestamp)] = {}
-          (1..6).each do |swell_index|
-            max_swell_rating = [max_swell_rating, responses.Sort["optimal#{swell_index}"][day_index][timestamp_index].to_d].max
+      responses.each do |response|
+        id = response.id
+        forecasts[id] ||= {}
+        response.Surf.dateStamp.each_with_index do |day, day_index|
+          day.each_with_index do |timestamp, timestamp_index|
+            ts = Time.zone.parse(timestamp)
+            forecasts[id][ts] ||= {}
+            forecasts[id][ts][:min_height] = response.Surf.surf_min[day_index][timestamp_index]
+            forecasts[id][ts][:max_height] = response.Surf.surf_max[day_index][timestamp_index]
           end
-          forecast[:swell_rating] = max_swell_rating
+        end
+
+        response.Sort.dateStamp.each_with_index do |day, day_index|
+          day.each_with_index do |timestamp, timestamp_index|
+            ts = Time.zone.parse(timestamp)
+            forecasts[id][ts] ||= {}
+            max_swell_rating = 0
+            (1..6).each do |swell_index|
+              max_swell_rating = [max_swell_rating, response.Sort["optimal#{swell_index}"][day_index][timestamp_index].to_d].max
+            end
+            forecasts[id][ts][:swell_rating] = max_swell_rating
+          end
+        end
+
+        response.Wind.dateStamp.each_with_index do |day, day_index|
+          day.each_with_index do |timestamp, timestamp_index|
+            ts = Time.zone.parse(timestamp)
+            forecasts[id][ts] ||= {}
+            forecasts[id][ts][:optimal_wind] = response.Wind.optimalWind[day_index][timestamp_index]
+          end
         end
       end
 
-      responses.Wind.dateStamp.each_with_index do |day, day_index|
-        day.each_with_index do |timestamp, timestamp_index|
-          forecast = forecasts[Time.zone.parse(timestamp)] = {}
-          forecast[:optimal_wind] = responses.Wind.optimalWind[day_index][timestamp_index]
+      forecasts.each do |surfline_id, timestamps|
+        timestamps.each do |timestamp, values|
+          record = unscoped.where(spot_id: Spot.find_by_surfline_id(surfline_id), timestamp: timestamp).first_or_initialize
+          record.api_request = request
+          values.each do |attribute, value|
+            record[attribute] = value
+          end
+          record.save!
         end
-      end
-
-      forecasts.each do |key, values|
-        record = unscoped.where(spot_id: spot.id, timestamp: key).first_or_initialize
-        record.api_request = request
-        values.each do |k, v|
-          record[k] = v
-        end
-        record.save!
       end
     end
   end
