7e409e351aa770b9338d259ef73afcb1e9741e12
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-04-11 16:24:30 -0700

    Improve height display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def height_range(min, max)
+    return unless min && max
+    "#{display_number(min)} - #{display_number(max)} ft"
+  end
+
+  def display_number(value)
+    number_with_precision(value, precision: 1, strip_insignificant_zeros: true)
+  end
 end
