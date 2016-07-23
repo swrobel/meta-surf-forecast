4f7c07bd4b1cac4c6053bd58afd360e25e0e8983
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-07-22 19:04:33 -0700

    Disable view rendering logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Disable view rendering logs
+  config.action_view.logger = nil
 end
