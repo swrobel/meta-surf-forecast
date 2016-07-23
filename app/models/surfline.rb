71e7ad0fdb5cd3b347b01b138cbfe6160ffc6179
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-07-22 17:45:44 -0700

    Interpolate surfline swell ratings

--- app/models/surfline.rb
+++ app/models/surfline.rb
@@ -37,35 +37,49 @@ class Surfline < Forecast
       forecasts = {}
 
       responses.each do |response|
-        id = response.id
-        forecasts[id] ||= {}
+        spot_id = response.id
+        forecasts[spot_id] ||= {}
         response.Surf.dateStamp.each_with_index do |day, day_index|
           day.each_with_index do |timestamp, timestamp_index|
-            ts = Time.zone.parse(timestamp)
-            forecasts[id][ts] ||= {}
-            forecasts[id][ts][:min_height] = response.Surf.surf_min[day_index][timestamp_index]
-            forecasts[id][ts][:max_height] = response.Surf.surf_max[day_index][timestamp_index]
+            tstamp = Time.zone.parse(timestamp)
+            forecasts[spot_id][tstamp] ||= {}
+            forecasts[spot_id][tstamp][:min_height] = response.Surf.surf_min[day_index][timestamp_index]
+            forecasts[spot_id][tstamp][:max_height] = response.Surf.surf_max[day_index][timestamp_index]
           end
         end
 
         response.Sort.dateStamp.each_with_index do |day, day_index|
           day.each_with_index do |timestamp, timestamp_index|
-            ts = Time.zone.parse(timestamp)
-            forecasts[id][ts] ||= {}
+            tstamp = Time.zone.parse(timestamp)
+            forecasts[spot_id][tstamp] ||= {}
             max_swell_rating = 0
             (1..6).each do |swell_index|
               max_swell_rating = [max_swell_rating, response.Sort["optimal#{swell_index}"][day_index][timestamp_index].to_d].max
             end
-            forecasts[id][ts][:swell_rating] = max_swell_rating
+            forecasts[spot_id][tstamp][:swell_rating] = max_swell_rating
           end
         end
 
         response.Wind.dateStamp.each_with_index do |day, day_index|
           day.each_with_index do |timestamp, timestamp_index|
-            ts = Time.zone.parse(timestamp)
-            forecasts[id][ts] ||= {}
-            forecasts[id][ts][:optimal_wind] = response.Wind.optimalWind[day_index][timestamp_index]
+            tstamp = Time.zone.parse(timestamp)
+            forecasts[spot_id][tstamp] ||= {}
+            forecasts[spot_id][tstamp][:optimal_wind] = response.Wind.optimalWind[day_index][timestamp_index]
           end
+        end
+      end
+
+      # Fill in blanks in swell ratings by averaging previous & next ratings
+      forecasts.keys.each do |spot_id|
+        spot_data = forecasts[spot_id]
+        # [1..-2] gets all elements except first & last
+        spot_data.keys[1..-2].each_with_index do |tstamp, index|
+          next unless spot_data[:swell_rating].blank?
+          prev_tstamp = spot_data.keys[index] # index is already offset by 1
+          next_tstamp = spot_data.keys[index + 2]
+          prev_rating = spot_data[prev_tstamp][:swell_rating]
+          next_rating = spot_data[next_tstamp][:swell_rating]
+          forecasts[spot_id][tstamp][:swell_rating] = (prev_rating + next_rating) / 2 if prev_rating && next_rating
         end
       end
 
