96e4148d47c78a8641803175b23d03388917e82d
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-08-17 16:12:36 -0700

    Improve page titles & FB metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def display_number(value)
     number_with_precision(value, precision: 1, strip_insignificant_zeros: true)
   end
+
+  def map_link(spot)
+    link_to 'map', spot.map_url
+  end
 end
