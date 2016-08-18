b7981ed5d5318e9096bbb09a5f44b052df5b01af
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-08-18 08:13:49 -0700

    Redirect Heroku to real Domain if present

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  constraints(host: /.herokuapp.com/) do
+    get '/(*path)' => redirect { |params, req| "https://#{ENV['HOST']}#{req.fullpath}" } if ENV['HOST'].present?
+  end
+
   resources :spots
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
