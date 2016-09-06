4f20a461cfe8d7e51d8aa1a80071cc19a224ea4b
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-09-06 09:08:35 -0700

    Disable all spot routes other than index/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
+
+# Serve websocket cable requests in-process
+# mount ActionCable.server => '/cable'
+
 Rails.application.routes.draw do
   constraints(host: /.herokuapp.com/) do
     get '/(*path)' => redirect { |_params, req| "https://#{ENV['HOST']}#{req.fullpath}" } if ENV['HOST'].present?
   end
 
-  resources :spots
-  # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
+  resources :spots, only: [:index, :show]
 
   root to: 'spots#index'
 end
