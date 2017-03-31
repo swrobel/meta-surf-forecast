9341df505b83aff608c4736ab971e870225f341f
Stefan Wrobel <swrobel@users.noreply.github.com>
2017-03-30 18:10:13 -0700

    Fix Puma config issue

--- config/puma.rb
+++ config/puma.rb
@@ -6,8 +6,6 @@ unless Rails.env.development?
   workers ENV['PUMA_WORKERS'] || 2
 end
 
-preload_app!
-
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
