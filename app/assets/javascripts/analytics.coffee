f0a1994c6fe8389dd7b7051c1c1fee7499e92114
Stefan Wrobel <swrobel@users.noreply.github.com>
2017-05-23 12:32:24 -0400

    Remove unneeded dependencies & files

--- app/assets/javascripts/analytics.coffee
+++
@@ -1,4 +0,0 @@
-document.addEventListener 'turbolinks:load', (event) ->
-  if typeof ga is 'function'
-    ga('set', 'location', event.data.url)
-    ga('send', 'pageview')
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery2
-//= require jquery_ujs
 //= require turbolinks
 //= require highcharts
 //= require chartkick
