1a63c16cafef523d1c5f2f232b0493f27a407b14
Stefan Wrobel <swrobel@users.noreply.github.com>
2017-06-18 18:44:00 -0700

    Use system default font in charts as god intended

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/FormatStringToken
+Chartkick.options = {
+  html: %(<div id="%{id}" style="height: %{height}; width: %{width}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: inherit;">Loading...</div>)
+}
