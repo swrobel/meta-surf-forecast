ad218f9fcbbb90150e698d35174bfd627b7fb638
Stefan Wrobel <swrobel@users.noreply.github.com>
2017-09-08 01:56:47 -0700

    Show longer forecasts for subregions that don't have Spitcast data

--- app/controllers/subregions_controller.rb
+++ app/controllers/subregions_controller.rb
@@ -2,89 +2,91 @@
 
 class SubregionsController < ApplicationController
   def show
-    @forecasts = Spot.connection.select_all <<-SQL
-      SELECT id
-            ,name
-            ,slug
-            ,lat
-            ,lon
-            ,msw_id
-            ,msw_slug
-            ,spitcast_id
-            ,spitcast_slug
-            ,surfline_id
-            ,surfline_slug
-            ,timestamp
-            ,round(min(min_height), 1) AS min
-            ,round(avg(avg_height) - min(min_height), 1) AS avg_delta
-            ,round(max(max_height) - avg(avg_height), 1) AS max_delta
-            ,max(max_height) AS max
-            ,round(avg(rating)) AS avg_rating
-        FROM
-        ( SELECT 'msw'
-                 ,spot_id
-                 ,timestamp
-                 ,min_height
-                 ,max_height
-                 ,(min_height + max_height) / 2 AS avg_height
-                 ,rating
-         FROM msws
-         UNION SELECT 'spitcast'
-                      ,spot_id
-                      ,timestamp
-                      ,height AS min_height
-                      ,height AS max_height
-                      ,height AS avg_height
-                      ,rating - 0.5 AS rating
-         FROM spitcasts
-         UNION SELECT 'lola'
-                      ,spot_id
-                      ,timestamp
-                      ,min_height
-                      ,max_height
-                      ,(min_height + max_height) / 2 AS avg_height
-                      ,(swell_rating * 5 * CASE WHEN optimal_wind THEN 1 ELSE 0.5 END) AS rating
-         FROM surfline_lolas
-         UNION SELECT 'nearshore'
-                      ,spot_id
-                      ,timestamp
-                      ,min_height
-                      ,max_height
-                      ,(min_height + max_height) / 2 AS avg_height
-                      ,(swell_rating * 5 * CASE WHEN optimal_wind THEN 1 ELSE 0.5 END) AS rating
-         FROM surfline_nearshores) sub
-        JOIN spots s ON sub.spot_id = s.id
-        WHERE sub.timestamp > now() at time zone 'utc'
-          AND sub.timestamp <= (SELECT max(timestamp) FROM spitcasts)
-          AND sub.rating IS NOT NULL
-          AND s.subregion_id = #{subregion.id}
-        GROUP BY id
-                ,name
-                ,slug
-                ,lat
-                ,lon
-                ,timestamp
-                ,surfline_id
-                ,surfline_slug
-                ,msw_id
-                ,msw_slug
-                ,spitcast_id
-                ,spitcast_slug
-        HAVING count(*) = CASE
-                            WHEN s.surfline_id IS NULL THEN 0
-                            ELSE 2
-                        END
-                        + CASE
-                          WHEN s.msw_id IS NULL THEN 0
+    @forecasts ||= Spot.connection.select_all <<-SQL
+      WITH
+      sub as ( SELECT 'msw' AS service
+               ,spot_id
+               ,timestamp
+               ,min_height
+               ,max_height
+               ,(min_height + max_height) / 2 AS avg_height
+               ,rating
+       FROM msws
+       UNION SELECT 'spitcast' AS service
+                    ,spot_id
+                    ,timestamp
+                    ,height AS min_height
+                    ,height AS max_height
+                    ,height AS avg_height
+                    ,rating - 0.5 AS rating
+       FROM spitcasts
+       UNION SELECT 'lola' AS service
+                    ,spot_id
+                    ,timestamp
+                    ,min_height
+                    ,max_height
+                    ,(min_height + max_height) / 2 AS avg_height
+                    ,(swell_rating * 5 * CASE WHEN optimal_wind THEN 1 ELSE 0.5 END) AS rating
+       FROM surfline_lolas
+       UNION SELECT 'nearshore' AS service
+                    ,spot_id
+                    ,timestamp
+                    ,min_height
+                    ,max_height
+                    ,(min_height + max_height) / 2 AS avg_height
+                    ,(swell_rating * 5 * CASE WHEN optimal_wind THEN 1 ELSE 0.5 END) AS rating
+       FROM surfline_nearshores)
+      SELECT
+         id
+        ,name
+        ,slug
+        ,lat
+        ,lon
+        ,msw_id
+        ,msw_slug
+        ,spitcast_id
+        ,spitcast_slug
+        ,surfline_id
+        ,surfline_slug
+        ,timestamp
+        ,round(min(min_height), 1) AS min
+        ,round(avg(avg_height) - min(min_height), 1) AS avg_delta
+        ,round(max(max_height) - avg(avg_height), 1) AS max_delta
+        ,max(max_height) AS max
+        ,round(avg(rating)) AS avg_rating
+      FROM sub
+      JOIN spots s ON sub.spot_id = s.id
+      WHERE sub.timestamp > now() at time zone 'utc'
+        AND sub.rating IS NOT NULL
+        AND s.subregion_id = #{subregion.id}
+      GROUP BY id
+              ,name
+              ,slug
+              ,lat
+              ,lon
+              ,timestamp
+              ,surfline_id
+              ,surfline_slug
+              ,msw_id
+              ,msw_slug
+              ,spitcast_id
+              ,spitcast_slug
+      HAVING count(*) = CASE
+                          WHEN s.surfline_id IS NULL THEN 0
+                          ELSE 2
+                      END
+                      + CASE
+                        WHEN s.msw_id IS NULL THEN 0
+                        ELSE 1
+                      END
+                      + CASE
+                          WHEN s.spitcast_id IS NULL THEN 0
                           ELSE 1
-                        END
-                        + CASE
-                            WHEN s.spitcast_id IS NULL THEN 0
-                            ELSE 1
-                        END
-        ORDER BY sort_order
-                ,id
-                ,timestamp
+                      END
+            AND min(sub.timestamp) <= (SELECT min(ts) FROM (SELECT max(timestamp) as ts FROM sub GROUP BY spot_id, service) s)
+      ORDER BY sort_order
+              ,id
+              ,timestamp
     SQL
     @forecasts.each(&:symbolize_keys!)
     @forecasts.each do |forecast|
