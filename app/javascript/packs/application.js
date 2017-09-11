7747e5c606ccf2b2e14791d10841e499bb6fae64
Stefan Wrobel <swrobel@users.noreply.github.com>
2017-09-11 16:18:39 -0700

    Slide header out of view when scrolling down, into view when scrolling up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,17 @@
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
+const componentRequireContext = require.context("components", true)
+const ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
+
+const Headroom = require("headroom.js")
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = document.querySelector(".navbar")
+  new Headroom(navbar, {
+    "classes": {
+      "initial": "animated",
+      "pinned": "slideDown",
+      "unpinned": "slideUp"
+    }
+  }).init()
+})
