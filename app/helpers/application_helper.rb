3c48af1be3116a2b42d1a795b505c799725b1332
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-01-26 18:35:39 -0800

    Spent way too much time making the blue navbar full-width on iPhone X

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
   end
 
   def map_link(target)
-    link_to image_tag('map.svg', alt: 'Map Icon'), target, target: 'gmap', title: 'Google Map'
+    link_to image_tag('map.svg', alt: 'Map Icon'), target, target: 'gmap', title: 'Google Map', class: 'icon'
   end
 
   def forecast_link(image_file, service, target, icon_label = nil)
-    link_to target, target: service, title: "#{service} Forecast Page", class: 'text-dark' do
+    link_to target, target: service, title: "#{service} Forecast Page", class: 'icon text-dark' do
       span_tag = tag.span(icon_label, class: 'forecast-icon-label') if icon_label
       image_tag(image_file, alt: "#{service} Logo") + span_tag
     end
