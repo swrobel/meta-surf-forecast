b86a60698930a1e49061126b7165363d2f3214d3
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-03-04 22:27:17 -0800

    Update rubocop & make it happy

--- lib/api_methods.rb
+++ lib/api_methods.rb
@@ -4,11 +4,12 @@ require 'open-uri'
 
 module ApiMethods
   def api_get(url)
-    response = JSON.parse(open(url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7').read, object_class: OpenStruct)
+    uri = URI.parse(url)
+    response = JSON.parse(uri.open('User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7').read, object_class: OpenStruct)
     request = ApiRequest.create(request: url, response: response, success: true)
-    return OpenStruct.new(request: request, response: response)
+    OpenStruct.new(request: request, response: response)
   rescue OpenURI::HTTPError => e
     ApiRequest.create(request: url, response: { message: e.message, meta: e.io.meta, status: e.io.status }, success: false)
-    return false
+    false
   end
 end
