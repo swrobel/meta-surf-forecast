ceb75d6d3524a905b137d5eda481b0b6a03154aa
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-03-17 11:39:29 -0700

    Update method to get name of called method for Ruby 2.0+

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,7 +4,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.raise_not_implemented_error
-    raise NotImplementedError, "Subclass should override method '#{caller(1..1).first[/`.*'/][1..-2]}'"
+    raise NotImplementedError, "Subclass should override method '#{caller_locations(1, 1)[0].label}'"
   end
   private_class_method :raise_not_implemented_error
 end
