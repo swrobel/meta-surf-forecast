0ef126140345ad88004985028792a12cbb16e04a
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-03-28 17:13:20 -0700

    Support for setting MSW session id for authenticated requests

--- lib/api_methods.rb
+++ lib/api_methods.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 module ApiMethods
   def api_get(url)
     uri = URI.parse(url)
-    response = JSON.parse(uri.open('User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7').read, object_class: OpenStruct)
+    response = JSON.parse(uri.open('User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7', 'Cookie' => "MSW_session=#{ENV['MSW_SESSION_ID']}").read, object_class: OpenStruct)
     request = ApiRequest.create(request: url, response: response, success: true)
     OpenStruct.new(request: request, response: response)
   rescue OpenURI::HTTPError => e
