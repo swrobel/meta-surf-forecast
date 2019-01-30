4d8fcbedddbdb3a24433ce44524589b8c1919a1d
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-01-29 16:48:22 -0800

    Clean up OpenURI call

--- lib/api_methods.rb
+++ lib/api_methods.rb
@@ -5,7 +5,11 @@ require 'open-uri'
 module ApiMethods
   def api_get(url)
     uri = URI.parse(url)
-    response = JSON.parse(uri.open('User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7', 'Cookie' => "MSW_session=#{ENV['MSW_SESSION_ID']}").read, object_class: OpenStruct)
+    open_uri_args = {
+      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7',
+      'Cookie' => "MSW_session=#{ENV['MSW_SESSION_ID']}"
+    }
+    response = JSON.parse(uri.open(open_uri_args).read, object_class: OpenStruct)
     request = ApiRequest.create(request: url, response: response, success: true)
     OpenStruct.new(request: request, response: response)
   rescue OpenURI::HTTPError => e
