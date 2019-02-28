f66accb25c88fa0324a63e0ed91a031da5e205f6
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-02-27 18:54:48 -0800

    Remove more complex log formatter that shows timestamp, log level, etc on every line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
