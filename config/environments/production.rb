e3186331929473551fa6f446a2fc5f8f5d04a14f
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-13 14:41:18 -0800

    Try simpler brotli instantiation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.insert_after ActionDispatch::Static, Rack::Brotli::Deflater
+  config.middleware.use Rack::Brotli::Deflater
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
