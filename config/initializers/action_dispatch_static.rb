96e81ef3b2be4a3dc790526b2cf39ee8ddfb3656
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-21 17:12:53 -0800

    Don't attempt to serve gzipped favicons

--- config/initializers/action_dispatch_static.rb
+++ config/initializers/action_dispatch_static.rb
@@ -4,7 +4,7 @@ ActionDispatch::FileHandler.class_eval do
   private
 
   def gzip_file_path(path)
-    return false if ['image/png', 'image/jpeg', 'image/gif'].include? content_type(path)
+    return false if %w[image/png image/jpeg image/gif image/x-icon image/vnd.microsoft.icon].include? content_type(path)
 
     gzip_path = "#{path}.gz"
     if File.exist?(File.join(@root, ::Rack::Utils.unescape_path(gzip_path)))
