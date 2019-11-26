7cdad2863b41e5b90a26913e43e9aad11f171d28
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-26 15:20:37 -0800

    Mark assets as immutable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.year.to_i}",
+    'Cache-Control' => "public, max-age=#{1.year.to_i}, immutable",
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
