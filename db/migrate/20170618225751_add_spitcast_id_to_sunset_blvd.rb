7e13a6eee45fed380d042569da39300d678030de
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-12-02 11:58:10 -0800

    Fix invalid migrations

--- db/migrate/20170618225751_add_spitcast_id_to_sunset_blvd.rb
+++
@@ -1,9 +0,0 @@
-class AddSpitcastIdToSunsetBlvd < ActiveRecord::Migration[5.1]
-  def up
-    Spot.find_by(surfline_id: 4883, msw_id: 3673).update_columns(spitcast_id: 387)
-  end
-
-  def down
-    Spot.find_by(surfline_id: 4883, msw_id: 3673).update_columns(spitcast_id: nil)
-  end
-end
