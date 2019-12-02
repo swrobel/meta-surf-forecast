279794f6633f8e6d871405609253919d17fc410e
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-12-02 12:21:29 -0500

    Remove heroku redirect

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ class ReservedWordConstraint
 end
 
 Rails.application.routes.draw do
-  constraints(host: /.herokuapp.com/) do
-    get '/(*path)' => redirect { |_params, req| "https://#{ENV['DOMAIN']}#{req.fullpath}" } if ENV['HOST'].present?
-  end
-
   get "/#{ENV['UNLOCK_KEY']}", to: 'application#unlock' if ENV['UNLOCK_KEY'].present?
 
   get '/regions/:id', to: redirect('/california/%{id}')
