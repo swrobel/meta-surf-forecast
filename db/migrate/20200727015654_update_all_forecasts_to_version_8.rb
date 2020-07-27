c47785ed06622afa73cc2e2571ee757901888aa2
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-07-26 18:58:24 -0700

    Exclude Surfline v1 rows with no swell or wind rating from all_forecasts view to match Rails scope behavior

---
+++ db/migrate/20200727015654_update_all_forecasts_to_version_8.rb
@@ -0,0 +1,15 @@
+class UpdateAllForecastsToVersion8 < ActiveRecord::Migration[6.0]
+  def up
+    drop_view :consolidated_forecasts, materialized: true
+    drop_view :all_forecasts, materialized: true
+    create_view :all_forecasts, version: 8, materialized: true
+    create_view :consolidated_forecasts, version: 2, materialized: true
+  end
+
+  def down
+    drop_view :consolidated_forecasts, materialized: true
+    drop_view :all_forecasts, materialized: true
+    create_view :all_forecasts, version: 7, materialized: true
+    create_view :consolidated_forecasts, version: 2, materialized: true
+  end
+end
