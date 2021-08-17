db966fc51a75923f3b2f9ed5d39e44a37d1a7493
Stefan Wrobel <swrobel@users.noreply.github.com>
2021-08-16 21:24:20 -0700

    Update Surfline V2 token request format

--- app/models/surfline_v2.rb
+++ app/models/surfline_v2.rb
@@ -2,23 +2,31 @@
 
 class SurflineV2 < Forecast
   self.abstract_class = true
+  AUTH_STRING = 'Basic NWM1OWU3YzNmMGI2Y2IxYWQwMmJhZjY2OnNrX1FxWEpkbjZOeTVzTVJ1MjdBbWcz'
   CACHE_KEY = 'surfline_token'
 
   class << self
     def access_token
       Rails.cache.fetch(CACHE_KEY) do
-        body = "grant_type=password&username=#{ENV['SURFLINE_EMAIL']}&password=#{ENV['SURFLINE_PASSWORD']}&device_id=Firefox-69.0&device_type=Firefox%2069.0%20on%20OS%20X%2010.14&forced=true"
+        body = {
+          authorizationString: AUTH_STRING,
+          grant_type: :password,
+          forced: true,
+          username: ENV['SURFLINE_EMAIL'],
+          password: ENV['SURFLINE_PASSWORD'],
+          device_id: 'Firefox-91.0',
+          device_type: 'Firefox 91.0 on OS X 10.15',
+        }.to_json
         headers = {
           'Accept' => 'application/json',
           'Accept-Language' => 'en-US,en;q=0.5',
-          'Authorization' => 'Basic NWM1OWU3YzNmMGI2Y2IxYWQwMmJhZjY2OnNrX1FxWEpkbjZOeTVzTVJ1MjdBbWcz',
-          'content-type' => 'application/x-www-form-urlencoded',
-          'credentials' => 'same-origin',
+          'Authorization' => AUTH_STRING,
+          'Content-Type' => 'application/json',
         }
         response = Typhoeus.post("#{SurflineV2.base_api_url}/trusted/token?isShortLived=false", headers: headers, body: body)
         raise "Surfline v2 access token refresh error: #{response.body}" unless response.success?
 
-        Logger.info 'Surfline v2 access token refreshed!'
+        logger.info 'Surfline v2 access token refreshed!'
 
         JSON.parse(response.body)['access_token']
       end
--- config/initializers/typhoeus.rb
+++ config/initializers/typhoeus.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-Typhoeus::Config.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:80.0) Gecko/20100101 Firefox/80.0'
+Typhoeus::Config.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0'
 API_RETRIES ||= ENV.fetch('API_RETRIES', 2)
