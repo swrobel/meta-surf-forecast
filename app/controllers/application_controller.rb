e2070d2bbb3210a9bb4a592d9274cd12e0dcd1b2
Stefan Wrobel <swrobel@users.noreply.github.com>
2023-02-10 12:25:07 -0800

    Revert "Remove cookie-based gate"
    This reverts commit bd36ae2418559f833e6791a7c136b15f636ff4da.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_action :check_unlocked, except: :unlock
+  before_action :check_unlocked, except: :unlock
 
   def unlock
     cookies[:unlock] = { value: Rails.application.credentials.unlock_secret, expires: 5.years.from_now }
