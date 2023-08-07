23aad57ed4a81c8c80065563c2c24fd8f8393695
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-08-07 07:01:00 -0700

    change seven_point_rating type to integer

--- db/migrate/20230718021133_add_seven_point_rating_to_surfline_v2s.rb
+++ db/migrate/20230718021133_add_seven_point_rating_to_surfline_v2s.rb
@@ -1,6 +1,6 @@
 class AddSevenPointRatingToSurflineV2s < ActiveRecord::Migration[7.0]
   def change
-    add_column :surfline_v2_lolas, :seven_point_rating, :decimal
-    add_column :surfline_v2_lotus, :seven_point_rating, :decimal
+    add_column :surfline_v2_lolas, :seven_point_rating, :integer
+    add_column :surfline_v2_lotus, :seven_point_rating, :integer
   end
 end
