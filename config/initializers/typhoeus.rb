1a0b8a465aa91b14c2777464e2140101c0e3a8f1
Stefan Wrobel <swrobel@users.noreply.github.com>
2025-04-23 08:35:00 -0700

    Really update user agent this time

--- config/initializers/typhoeus.rb
+++ config/initializers/typhoeus.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-Typhoeus::Config.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0'
+Typhoeus.configure do |config|
+  config.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:137.0) Gecko/20100101 Firefox/137.0'
+end
 API_RETRIES ||= ENV.fetch('API_RETRIES', 2)
