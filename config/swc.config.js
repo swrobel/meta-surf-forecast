6cf7e4823f18c44298bd3704ea6a281ca842fc73
Stefan Wrobel <swrobel@users.noreply.github.com>
2025-12-14 14:54:39 -0800

    Migrate from webpack to rspack

--- config/swc.config.js
+++ config/swc.config.js
@@ -2,12 +2,4 @@
 // This file is merged with Shakapacker's default SWC configuration
 // See: https://swc.rs/docs/configuration/compilation
 
-module.exports = {
-  jsc: {
-    transform: {
-      react: {
-        runtime: "automatic"
-      }
-    }
-  }
-}
+module.exports = {}
